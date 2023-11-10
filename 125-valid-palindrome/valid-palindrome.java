7d5e620950d9511365210f418f64eec0d251a62c
Meghanath Reddy M C <116367208+MeghanathMC@users.noreply.github.com>
2023-11-10 20:04:39 +0530

    Time: 2 ms (99.36%) | Memory: 41.7 MB (86.79%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+       int left=0;
+       int right=s.length()-1;
+       while(left<right){
+           char l=s.charAt(left), r=s.charAt(right);
+           if(!Character.isLetterOrDigit(l)){
+               left++;
+           }
+           else if(!Character.isLetterOrDigit(r)){
+               right--;
+           }
+           else if(Character.toLowerCase(l)!=Character.toLowerCase(r)){
+               return false;
+           }
+           else{
+               left++;
+               right--;
+           }
+       }
+       
+       return true;
+        
+    }
+}
