03ba13766456a5e1c963c1f51b43203cead51ca1
Meghanath Reddy M C <116367208+MeghanathMC@users.noreply.github.com>
2024-09-29 10:37:44 +0530

    Time: 1 ms (100.00%) | Memory: 57.3 MB (48.03%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -3,20 +3,16 @@ class Solution {
        int ans=1;
       int i=0;
       while(i<nums.length){
-        if(nums[i]>0 && nums[i]<nums.length){
-            if(nums[i]!=nums[nums[i]-1]){
+        if(nums[i]>0 && nums[i]<nums.length && nums[i]!=nums[nums[i]-1]){
+            
                 int temp=nums[i];
                 nums[i]=nums[temp-1];
                 nums[temp-1]=temp;
-            }
+        }
             else{
                 i++;
 
             }
-        }
-        else{
-        i++;
-        }
       }
 
       for(int j=0;j<nums.length;j++){
